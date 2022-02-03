56f249446225396f79daf3aac4beffd7c30efac6
Mahdy Noureddine <mahdy.a.noureddine@gmail.com>
2022-02-03 17:07:50 -0500

    commit tree 523cf867efc47aaee7700b496b04ec417ede1d32

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,17 @@
 class UsersController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    def show
+        user = find_user
+        render json: user, include: :items, except: [:created_at, :updated_at], status: :ok
+    end
+
+    private
+
+    def find_user
+        User.find(params[:id])
+    end
+
+    def render_not_found_response
+        render json: {error: "User not found"}, status: :not_found
+    end
 end
